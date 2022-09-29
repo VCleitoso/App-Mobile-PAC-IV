5ca31a5d1b8942177898b89939fecf8c8af0c9ab
Bruno Ceresér <bruno.cereser@catolicasc.edu.br>
2022-09-28 21:30:52 -0300

    Update HelpPage.dart
    Fundo e appbar agora utilizam as variáveis de design em main

--- pac4/lib/HelpPage.dart
+++ pac4/lib/HelpPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frases_aletorias_app/main.dart';
 
 class HelpPage extends StatelessWidget{
 
@@ -7,18 +8,42 @@ class HelpPage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      backgroundColor: fundoCor,
       appBar: AppBar(
         centerTitle: false,
-        title: const Text("PAC IV"),
-        backgroundColor: Colors.grey,
+        title: const Text("MyMeal"),
+        backgroundColor: appbarCor,
         //actions: [],//Coisas a direita
       ),
-      body: Center(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(image: AssetImage('logoMyMeal.png'),
+          fit: BoxFit.contain),
+        ),
         child: Padding(
           padding: const EdgeInsets.all(10),
           child: ListView(
-            children: const [
-              Text("Chama o supervisor lá."),
+            children: [
+
+              //TÍTULO GRANDÃO
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10),
+                  child: const Text(
+                    "Ajuda",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 30,
+                    ),
+                  )
+              ),
+
+              const Text(
+                "Chama o supervisor lá.",
+                style: TextStyle(
+                  fontSize: 50,
+              ),),
 
             ],
           ),
