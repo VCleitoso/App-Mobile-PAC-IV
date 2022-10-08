8365dbe0df0c9a52134a1fecc636c0c77dc5969a
Júlio Spezzia de Souza <julim.generic@gmail.com>
2022-10-08 15:13:49 -0300

    Teste de push
    Botei uns botões no admin

--- pac4/lib/AdminCadastrarFuncionario.dart
+++ pac4/lib/AdminCadastrarFuncionario.dart
@@ -4,12 +4,15 @@ import 'main.dart';
 class AdminCadastrarFuncionario extends StatefulWidget{
   const AdminCadastrarFuncionario({Key? key}) : super(key: key);
 
+
   @override
   State<AdminCadastrarFuncionario> createState() => AdminCadastrarFuncionarioInstance();
 }
 
 class AdminCadastrarFuncionarioInstance extends State<AdminCadastrarFuncionario>{
-
+  final userController = TextEditingController();
+  final passwordController = TextEditingController();
+  final codeController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,6 +44,59 @@ class AdminCadastrarFuncionarioInstance extends State<AdminCadastrarFuncionario>
                         fontSize: 30,
                       ),
                     )
+                ),
+                //Inserir Número do Funcionário
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    controller: codeController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Número do novo Funcionário",
+                      hintText: "Ex: 10808",
+                    ),
+                  ),
+                ),
+
+                //Inserir nome
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    controller: userController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Nome do novo funcionario",
+                      hintText: "Digite o nome",
+                    ),
+                  ),
+                ),
+
+                //Inserir senha
+                Container(
+                  //Alterar para baixar lista de sugestões
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    controller: passwordController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Senha",
+                      ),
+                  ),
+                ),
+
+                //Botão de Salvar
+                Container(
+                  height: 50,
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(botaoCor)
+                    ),
+                    child: const Text('Salvar'),
+                    onPressed: () {
+
+                    }, //onPressed
+                  ),
                 ),
 
               ] // Children
